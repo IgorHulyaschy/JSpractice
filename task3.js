b3db79976d2bf13e95e2db50213703c12ae9cd85
IgorHulyaschy <gulyaschy@gmail.com>
2021-03-11 00:29:01 +0200

    update task3

--- task3.js
+++ task3.js
@@ -1,7 +1,7 @@
 // Find letters in ukranian words that are written the same as English ones. 'Дерево' -> ['e', 'p', 'o']
 const result = [];
-const word = "україна";
-const letters = ["а", "с", "е", "р", "і", "о", "х", "у"];
+const word = "уКраїна";
+const letters = ["а", "с", "е", "р", "і", "о", "х", "у", "А", "В", "С", "Н", "І", "К", "М", "О", "Р", "Т", "Х"];
 for (let i = 0; i < word.length; i++) {
   if (letters.includes(word[i])) result.push(word[i]);
 }
